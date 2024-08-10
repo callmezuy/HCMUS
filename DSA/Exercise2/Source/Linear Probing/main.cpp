297a32c338c2298b98c5bf11901e1b1daec6df23
callmezuy <mduy2204@gmail.com>
2024-08-10 14:50:49 +0700

    nothin

--- DSA/Exercise2/Source/Linear Probing/main.cpp
+++ DSA/Exercise2/Source/Linear Probing/main.cpp
@@ -2,29 +2,4 @@
 
 #include "hash.h"
 
-int main() {
-  HashTable<int, string> hashTable;
-  hashTable.init(10);
-
-  hashTable.add(1, "one");
-  hashTable.add(2, "two");
-  hashTable.add(3, "three");
-  hashTable.add(4, "four");
-  hashTable.add(5, "five");
-
-  cout << *hashTable.searchValue(1) << endl;
-  cout << *hashTable.searchValue(2) << endl;
-  cout << *hashTable.searchValue(3) << endl;
-  cout << *hashTable.searchValue(4) << endl;
-  cout << *hashTable.searchValue(5) << endl;
-
-  hashTable.removeKey(3);
-  if (hashTable.searchValue(3) != nullptr) {
-    cout << *hashTable.searchValue(3) << endl;
-  } else {
-    cout << "Key 3 not found" << endl;
-  }
-
-  hashTable.release();
-  return 0;
-}
+int main() { return 0; }
